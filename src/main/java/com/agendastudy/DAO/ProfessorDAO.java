4d060907efb9ab9a28e1e69beccc99a735fd86a6
Vinicius Ribeiro <vinir14.p5@gmail.com>
2025-11-12 20:45:22 -0300

    classe ProfessorDAO criada e classe EstudanteDAO modificada

---
+++ src/main/java/com/agendastudy/DAO/ProfessorDAO.java
@@ -0,0 +1,14 @@
+package com.agendastudy.DAO;
+public class ProfessorDAO {
+    
+    //IMPLEMENTAR DEMAIS MÉTODOS AQUI
+
+    public void solicitarCancelamento(String idAula, AulaDAO aulaDAO){
+        try{
+            aulaDAO.cancelarAula(idAula);
+            System.out.println("Cancelamento solicitado.");
+        } catch (Exception e){
+            System.err.println("Erro ao cancelar aula: " + e.getMessage());
+        }
+    }
+}
