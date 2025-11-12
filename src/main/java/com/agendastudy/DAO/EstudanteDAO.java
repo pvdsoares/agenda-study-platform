304d09e327fdbc75086c6c0aae0736864c4565b9
Vinicius Ribeiro <vinir14.p5@gmail.com>
2025-11-12 00:04:38 -0300

    solicitar cancelamento de aula

--- src/main/java/com/agendastudy/DAO/EstudanteDAO.java
+++ src/main/java/com/agendastudy/DAO/EstudanteDAO.java
@@ -2,4 +2,14 @@ package com.agendastudy.DAO;
 
 public class EstudanteDAO {
    
+    // IMPLEMENTAÇÃO DE DEMAIS MÉTODOS AQUI
+
+    public void solicitarCancelamento(String idAula, AulaDAO aulaDAO){
+        try{
+            aulaDAO.cancelarAula(idAula);
+            System.out.println("Cancelamento solicitado.");
+        } catch (Exception e){
+            System.err.println("Erro ao cancelar aula: " + e.getMessage());
+        }
+    }
 }
