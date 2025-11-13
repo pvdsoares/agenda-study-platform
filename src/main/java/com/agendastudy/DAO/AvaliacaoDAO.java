8a2eec7fb359fd74ac47152990ad9bb89fefd7bf
Matheus Pereira Rodrigues <mts027@gmail.com>
2025-11-13 14:47:39 -0300

    Adiociona classe AvaliacaoDAO (DAO)

---
+++ src/main/java/com/agendastudy/DAO/AvaliacaoDAO.java
@@ -0,0 +1,80 @@
+package com.agendastudy.DAO;
+import com.agendastudy.model.Aula;
+import com.agendastudy.model.Avaliacao;
+import com.agendastudy.model.Estudante;
+import com.agendastudy.model.Professor;
+
+import java.time.LocalDateTime;
+import java.util.*;
+
+
+public class AvaliacaoDAO {
+    private Map<Professor, List<Avaliacao>> avaliacoesPorProfessor = new HashMap<>();
+
+    // Com comentário
+    public void avaliar(Estudante estudante, Aula aula, int nota, String comentario) {
+        if (comentario == null) {
+            comentario = "";
+        }
+        avaliarInterno(estudante, aula, nota, comentario);
+    }
+
+    // Sem comentário
+    public void avaliar(Estudante estudante, Aula aula, int nota) {
+        avaliarInterno(estudante, aula, nota, "");
+    }
+
+    /**
+     * Avalia uma aula já concluída e ainda não avaliada
+     *
+     * @param estudante que irá avaliar a aula
+     * @param aula a ser avaliada
+     * @param nota nota da avaliação
+     * @param comentario comentário da avaliação
+     */
+    private void avaliarInterno(Estudante estudante, Aula aula, int nota, String comentario) {
+        if (podeAvaliar(estudante, aula)) {
+            String idAvaliacao = UUID.randomUUID().toString(); //Gera novo ID para cada avaliação
+            Avaliacao avaliacao = new Avaliacao(idAvaliacao, estudante, aula, nota, comentario);
+
+            //Se não existir uma lista para o professor é criada uma nova
+            avaliacoesPorProfessor.putIfAbsent(aula.getProfessor(), new LinkedList<>());
+
+            //Adiciona a avaliação a lista do professor
+            avaliacoesPorProfessor.get(aula.getProfessor()).add(avaliacao);
+        }
+    }
+
+    /**
+     * Verifica se aula pode ser avaliada
+     *
+     * @param estudante que irá avaliar a aula
+     * @param aula a ser avaliada
+     * @return true se a aula pode ser avaliada, false caso contrário
+     */
+    public boolean podeAvaliar(Estudante estudante, Aula aula) {
+        //se aula ainda não aconteceu, não pode ser avaliada
+        if (aula.getDataHora().isAfter(LocalDateTime.now())) {
+            return false;
+        }
+
+        //se aula cancelada, não pode ser avaliada
+        if (aula.isCancelada()) {
+            return false;
+        }
+
+        //pega as avaliações do professor da aula a ser verificada (ou lista vazia, se não existir)
+        List<Avaliacao> avaliacoesDoProfessor = avaliacoesPorProfessor
+                .getOrDefault(aula.getProfessor(), Collections.emptyList());
+
+        //verifica apenas as avaliações desse professor
+        for (Avaliacao a : avaliacoesDoProfessor) {
+            if (a.getAula().getIdAula().equals(aula.getIdAula()) &&
+                    a.getEstudante().getId().equals(estudante.getId())) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
